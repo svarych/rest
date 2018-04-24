d21e6fe1e14e45588b429c67c6e21a23e2f9d334
Сварич Артем Віталійович <svarych.a@novaposhta.ua>
2018-04-24 15:53:17 +0300

    Allure report generates/ Publish experiment #14 - Screenshots

--- src/test/java/integration/GoogleTests.java
+++ src/test/java/integration/GoogleTests.java
@@ -4,32 +4,31 @@ import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
-import static com.codeborne.selenide.Condition.*;
-import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.$$;
-import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byName;
+import static com.codeborne.selenide.Selenide.*;
 
 public class GoogleTests {
 
     @BeforeAll
-    static void setUpBrowser(){
+    static void setUpBrowser() {
         Configuration.browser = "phantomjs";
         Configuration.browserSize = "1920x1080";
         Configuration.holdBrowserOpen = true;
-        Configuration.reportsFolder="./target/surefire-reports";
+        Configuration.reportsFolder = "./target/surefire-reports";
     }
 
     @Test
-    void googleTestShouldBeGreen(){
+    void googleTestShouldBeGreen() {
         open("http://google.com");
-        $(".gsfi").setValue("selenide").pressEnter();
+        $(byName("q")).val("selenide").pressEnter();
         $$(".g").get(0).shouldHave(text("Selenide: лаконичные и стабильные UI тесты на Java"));
     }
 
     @Test
-    void googleTestShouldBeRed(){
+    void googleTestShouldBeRed() {
         open("http://google.com");
-        $(".gsfi").setValue("selenide").pressEnter();
+        $(byName("q")).val("selenide").pressEnter();
         $$(".g").get(0).shouldHave(text("Какие преимущества даёт Selenide"));
     }
 
