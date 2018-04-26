b47ee5c4141a9d93c71c3a4b78256b7b4e04c991
Сварич Артем Віталійович <svarych.a@novaposhta.ua>
2018-04-26 10:40:15 +0300

    Allure report generates/ Publish experiment #20 - Screenshots (Allure + Selenide) - path changed to allure-results

--- Jenkinsfile
+++ Jenkinsfile
@@ -10,7 +10,7 @@ pipeline {
 
             steps {
                 sh 'mvn clean test -Dmaven.test.failure.ignore=true'
-                allure includeProperties: false, jdk: '', results: [[path: 'target/surefire-reports/']]
+                allure includeProperties: false, jdk: '', results: [[path: 'allure-results']]
             }
         }
     }
--- src/test/java/integration/GoogleTests.java
+++ src/test/java/integration/GoogleTests.java
@@ -4,6 +4,7 @@ import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.logevents.SelenideLogger;
 import io.qameta.allure.Attachment;
 import io.qameta.allure.Step;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import com.codeborne.selenide.junit.ScreenShooter;
@@ -15,44 +16,30 @@ import static com.codeborne.selenide.Selenide.*;
 
 public class GoogleTests {
 
-    // TODO реализовать отображение скриншотов в Allure
     @BeforeEach
     void setUp() {
         System.setProperty("junit.jupiter.extensions.autodetection.enabled", "true");
         SelenideLogger.addListener("allure", new AllureSelenide());
     }
 
-//    @BeforeAll
-//    static void setUpBrowser() {
-//        Configuration.browser = "phantomjs";
-//        Configuration.browserSize = "1920x1080";
-//        Configuration.holdBrowserOpen = true;
-////        Configuration.reportsFolder = "./target/surefire-reports";
-//    }
-//
-//    @Test
-//    void googleTestShouldBeGreen() {
-//        open("http://google.com");
-//        $(byName("q")).val("selenide").pressEnter();
-//        $$(".g").get(0).shouldHave(text("Selenide: лаконичные и стабильные UI тесты на Java"));
-//    }
-//
-//    @Test
-//    void googleTestShouldBeRed() {
-//        open("http://google.com");
-//        $(byName("q")).val("selenide").pressEnter();
-//        $$(".g").get(0).shouldHave(text("Какие преимущества даёт Selenide"));
-//    }
+    @BeforeAll
+    static void setUpBrowser() {
+        Configuration.browser = "phantomjs";
+        Configuration.browserSize = "1920x1080";
+        Configuration.holdBrowserOpen = true;
+    }
 
     @Test
-    @Step
-    @Attachment(type = "image/png")
-    void t0() {
-        Configuration.browser = "phantomjs";
+    void googleTestShouldBeGreen() {
         open("http://google.com");
         $(byName("q")).val("selenide").pressEnter();
-//        $$(".g").get(0).shouldHave(text("Selenide: лаконичные и стабильные UI тесты на Java"));
-        $$(".g").get(0).shouldHave(text("Что-то пошло не так"));
-//        ScreenShooter.failedTests().to("./allure-report/screenshots");
+        $$(".g").get(0).shouldHave(text("Selenide: лаконичные и стабильные UI тесты на Java"));
+    }
+
+    @Test
+    void googleTestShouldBeRed() {
+        open("http://google.com");
+        $(byName("q")).val("selenide").pressEnter();
+        $$(".g").get(0).shouldHave(text("Какие преимущества даёт Selenide"));
     }
 }
